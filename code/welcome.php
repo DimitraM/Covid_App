08f8f1ac9f869e056452855f1557b79a8e326df2
DimitraM <54402458+DimitraM@users.noreply.github.com>
2022-09-10 01:59:01 +0300

    Update welcome.php

--- code/welcome.php
+++ code/welcome.php
@@ -17,8 +17,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <title>Welcome Page</title>
 
 
-	<link rel="stylesheet" type="text/css" href="dropdown_menu.css">
-	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href ="bootstrap/css/bootstrap.min.css">
@@ -27,96 +25,118 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
      <!-- Geolocation css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol/dist/L.Control.Locate.min.css" />
 
-    <link href="dropdown_menu.css" rel="stylesheet">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
+
+
 
     <style>
         /*gia na mhn exei lefka shmeia sthn othoni*/
         body{
             margin: 0;
             padding: 0;
+            background-color: #d1e8e2;
         }
+
         /* thelw na pianei olh thn othoni*/
         #map{
-            position: absolute;
-            width: 100%;
-            height: 80vh;
+           height:600px;
+           width: 95%;
+           top: 150px;
+           left: 2.5%;
+           border-radius: 5px;
+
+
 
         }
         .input-group{
-            position: absolute;
+            position: fixed;
             padding: 10px;
-            top: 50;
+            top:25px;
             width: 20%;
             z-index: 9999;
-            left:40%;
+            right: 2%;
         }
+        .footer{
+          position:absolute;
+          bottom: 0;
+          z-index: 9999;
+          height: 5px;
+        }
+        .navbar{
+          position: fixed;
+          top: 0;
+          width: 100%;
+          height:14vh;
+          z-index: 9999;
 
+        }
+        .navbar-brand{
+          padding: 50px;
+          left: 6px;
+        }
+        .navbar-nav{
+          padding: 15px;
+
+        }
     </style>
+
+
 </head>
 <body>
 
 <!--Menu bar-->
 
-<header class="p-3 mb-3 border-bottom">
-  <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-      <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-      </a>
-
-      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="login.php" class="nav-link px-2 link-secondary">Καταχώρηση Κρούσματος</a></li>
-        <li><a href="login.php" class="nav-link px-2 link-dark">Πιθανή επαφή με κρούσμα</a></li>
-        <div class="dropdown text-end">
-      </ul>
-    <div class="wrapper">
-  <div class="dropdown dropdown_desktop_icon">
-      <button><span class="icon"></span>
-        <span class="text">User Menu</span>
-      </button>
-      <ul>
-        <li><a href="#">
-          <span class="icon">
-            <ion-icon name="person-sharp"></ion-icon>
-          </span>
-          <span class="text">Επεξεργασία Προφιλ</span>
-        </a></li>
-        <li><a href="register.php">
-          <span class="icon">
-            <ion-icon name="help-circle-sharp"></ion-icon>
-          </span>
-          <span class="text">Log out</span>
-        </a></li>
-
-  </div>
-</div>
-
-</ul>
+  <nav id ="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+    <a id="navbar_title" class="navbar-brand" href="welcome.php">Covid-19 Tracking</a>
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <!-- Na ftiaxoume covid_case.php  -->
+            <!-- class="nav-link active" me ayto to kanoume akrive -->
+            <!-- aria-current="page" me ayto deixnoume oti eimaste sthn sugkekrimenh selida -->
+            <a class="nav-link" href="covid_case.php">Καταχώρηση Κρούσματος</a>
+          </li>
+          <li class="nav-item">
+            <!-- Na ftisxoume load covid.php -->
+            <a class="nav-link" href="load_covid.php">Πιθανή επαφή με κρούσμα</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Επεξεργασία Προφιλ
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="change_credentials.php">Αλλαγή Στοιχείων</a></li>
+              <li><a class="dropdown-item" href="history.php">Ιστορικό</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+            </ul>
+          </li>
         </ul>
+        <!-- <form class="d-flex"> -->
+        <!-- <div class="input-group">
+          <input id="search_input" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button id="search_btn" class="btn btn-outline-success" type="submit">Search</button>
+        </div> -->
+        <!-- </form> -->
       </div>
     </div>
+  </nav>
+<!-- searchbar -->
+  <div class="input-group">
+    <input id="search_input" type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+    <button id="search_btn" type="submit" value="Submit" class="btn btn-dark">Search</button>
   </div>
-</header>
-
+<!-- Map div -->
+  <div id="map"></div>
 </div>
-
-      <div class="input-group">
-        <input id="search_input" type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-        <button id="search_btn" type="submit" value="Submit" class="btn btn-dark">Search</button>
-      </div>
-
-
-      <div id="map"></div>
-
-      <footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-    <span class="text-muted">
-	Η τρέχουσα διαδικτυακή εφαρμογή αναπτύχθηκε στα πλαίσια του μαθήματος "Προγραμματισμός και Συστήματα στον Παγκόσμιο Ιστό
-    <br>Copyright 2022 &copy</span>
-  </div>
-</footer>
-
+<!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">
+  	Η τρέχουσα διαδικτυακή εφαρμογή αναπτύχθηκε στα πλαίσια του μαθήματος "Προγραμματισμός και Συστήματα στον Παγκόσμιο Ιστό
+      <br>Copyright 2022 &copy</span>
+    </div>
+  </footer>
 
 </body>
 
@@ -130,8 +150,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <script src="lib/leaflet/jquery-3.5.1.js"></script>
 
 <script src="js/source_code.js"></script>
-
-<script type="text/javascript" src="js/dropdowns.js"></script>
 
 <!-- Control Locate-->
 <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol/dist/L.Control.Locate.min.js" charset="utf-8"></script>
